afaaa8489318a1bece4bbf5eae2cbc9dfa68bf72
Ana Maria Brito Souza <160272076+soouzaana@users.noreply.github.com>
2026-03-31 10:17:55 -0400

    Adicionar arquivos de projeto.

---
+++ Atividade2/Classes/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ProjetoLanchonete.Classes
+{
+    public class Pedido
+    {
+        private string nomeCliente;
+        private double valor;
+        private double taxaEntrega;
+
+        public Pedido(string nomeCliente, double valor)
+        {
+            this.nomeCliente = nomeCliente;
+            this.valor = valor;
+            this.taxaEntrega = 0;
+        }
+
+        public Pedido(string nomeCliente, double valor, double taxaEntrega)
+        {
+            this.nomeCliente = nomeCliente;
+            this.valor = valor;
+            this.taxaEntrega = taxaEntrega;
+        }
+
+        public double CalcularTotal()
+        {
+            return valor + taxaEntrega;
+        }
+
+        public void ExibirDetalhes()
+        {
+            Console.WriteLine("---------- DETALHES DO PEDIDO ----------");
+            Console.WriteLine($"Cliente: {nomeCliente}");
+            Console.WriteLine($"Valor do pedido: R${valor:F2}");
+            if (taxaEntrega > 0)
+            {
+                Console.WriteLine($"Taxa de entrega: R${taxaEntrega:F2}");
+            }
+            Console.WriteLine($"Total a pagar: R${CalcularTotal():F2}");
+            Console.WriteLine("-----------------------------------------");
+        }  
+
+    }
+}
---
+++ Atividade2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ProjetoLanchonete.Classes;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Valor do pedido: ");
+        double valor = double.Parse(Console.ReadLine());
+
+        Console.Write("Deseja entrega (s/n)? ");
+        string entrega = Console.ReadLine().ToLower();
+
+        Console.Clear();
+
+        Pedido pedido;
+
+        if(entrega == "s") {
+            Console.Write("Digite a taxa de entrega: ");
+            double taxa = double.Parse(Console.ReadLine());
+
+            pedido = new Pedido(nome, valor, taxa);
+        }
+        else
+        {
+            pedido = new Pedido(nome, valor);
+        }
+
+        Console.Clear();
+
+        pedido.ExibirDetalhes();
+    }
+}
